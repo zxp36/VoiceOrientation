b421984808bbff7375ca7f61abc7ddea17ea6f36
故事的小红星 <15515773535@163.com>
2019-12-19 20:37:38 +0800

    Update iotTream2.sh

--- VoiceOrientation/luoly/iotTream2.sh
+++ VoiceOrientation/luoly/iotTream2.sh
@@ -8,11 +8,14 @@
 # 测试的时候空缺的数据
 
 
-vcomef="$1"						#机器或者人声
-anvcomef=""
+vcomef="$1"	#机器或者人声，其中vcomef变量的意思是 voice come from 也就是声源的类型，“$1”在Linux中代表传给脚本的第一个参数
 
 
-if [ "$#" != 1 ]; then
+
+anvcomef=""	#anvcomef 变量的含义为 another vocie come from, 代表的是另外一个声源类型
+
+
+if [ "$#" != 1 ]; then   #‘$#’代表传给脚本的参数个数
 	echo "Please input source of the voice: "m" or "h" "
 	exit 1
 fi
@@ -21,15 +24,16 @@ fi
 cd sound 
 rm -rf log
 mkdir log
-python kws_cch.py
+python kws_cch.py 	#该python脚本的作用是开始在四麦克风阵列上接收音频，产生后缀为.wav的音频文件
 cd ..
 rm log
-python generatePar.py sound/log/test
-python generateResult.py sound/log/test
+python generatePar.py sound/log/test 	#该步的作用是调用MAUS技术对收集到.wav文件进行切割，生成需要的TextGrid文件
+python generateResult.py sound/log/test		#该步的作用在每个麦克风处是产生TDoA的值，均为该麦克风与麦克风2号之间的TDoA的值
 
 
-# classify
+# classify，接下来的作用是判定我们所接收到的声音的声源类型
 
+#下面这个if语句的作用是为变量anvcomef进行赋值操作
 
 if [ "$vcomef" = "h" ]; then
 	anvcomef="machine"
@@ -37,7 +41,7 @@ elif [ "$vcomef" = "m" ]; then
 	anvcomef="human"
 fi
 
-sh sort2_sh.sh log log_${anvcomef} $vcomef
+sh sort2_sh.sh log log_${anvcomef} $vcomef 
 
 cp log_human.human log_machine.machine ../libsvm/python
 cd ../libsvm/python
